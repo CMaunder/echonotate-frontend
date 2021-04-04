3aff85b6065ccf33da9f0ef68a8bff1296b0429c
Charlie Maunder <cmaunderc@gmail.com>
2021-04-04 09:41:56 +0100

    aws bucket upload working!

--- pages/api/upload-audio-file.ts
+++ pages/api/upload-audio-file.ts
@@ -1,16 +1,25 @@
-import formidable from 'formidable';
+import aws from 'aws-sdk';
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-export default async (req, res) => {
-  const form = new formidable.IncomingForm();
-  form.uploadDir = "./audio/";
-  form.keepExtensions = true;
-  form.parse(req, (err, fields, files) => {
-    console.log(err, fields, files);
+export default async function handler(req, res) {
+  aws.config.update({
+    accessKeyId: process.env.ACCESS_KEY_2,
+    secretAccessKey: process.env.SECRET_KEY_2,
+    region: process.env.REGION,
+    signatureVersion: 'v4',
   });
-};
+
+  const s3 = new aws.S3();
+  const post = await s3.createPresignedPost({
+    Bucket: process.env.BUCKET_NAME,
+    Fields: {
+      key: req.query.file,
+    },
+    Expires: 60, // seconds
+    Conditions: [
+      ['content-length-range', 0, 1048576], // up to 1 MB
+    ],
+  });
+  console.log(post)
+
+  res.status(200).json(post);
+}
