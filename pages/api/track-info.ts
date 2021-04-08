e335c7457b0445d2611dddec1ed71cc9c5748d4a
Charlie Maunder <charlie.maunder@fdmgroup.com>
2021-04-08 20:15:34 +0100

    adding cors

--- pages/api/track-info.ts
+++ pages/api/track-info.ts
@@ -3,9 +3,21 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { predictNotesUrl } from '../../config/index'
 import jwt from 'jsonwebtoken'
 import axios from "axios";
+import Cors from 'cors'
+import initMiddleware from '../../lib/init-middleware'
+
+const cors = initMiddleware(
+  Cors({
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  await cors(req, res)
   const { db } = await connectToDatabase();
+  res.setHeader('Access-Control-Allow-Origin', '*'); // to enable calls from every domain 
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE'); // allowed actiosn
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
 
   if (req.method === 'PUT') {
     const objToSave = {... req.body, created: new Date()}
@@ -14,7 +26,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     .insertOne(objToSave);
     res.status(201).json(response.insertedId)
   } else if (req.method === 'GET') {
-    console.log(req.body)
+    //console.log(req.body)
     // if the track is uploaded to s3, get the predicted notes
     const trackInfo = {
       key: 'c-major-scale.mp3',
